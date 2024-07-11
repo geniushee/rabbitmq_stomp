bdf9c1d60f4f1b0b325a43b79a8e988e33e2aae2
geniushee <h1116jeon@gmail.com>
2024-07-11 16:21:26 +0900

    git init

---
+++ src/main/java/com/example/rabbitmq_stomp/RabbitmqStompApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq_stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqStompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqStompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabbitmq_stomp/RabbitmqStompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq_stomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqStompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
